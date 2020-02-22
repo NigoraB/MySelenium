90c0b31d97bf94f6af76bff4eb2ca181d31139db
NigoraB <52468276+NigoraB@users.noreply.github.com>
2020-02-22 16:00:04 -0600

    Add files via upload

---
+++ FileUploadPopup.java
@@ -0,0 +1,26 @@
+package SeleniumSessions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FileUploadPopup {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://cgi-lib.berkeley.edu/ex/fup.html");
+driver.findElement(By.name("upfile")).sendKeys("/Users/nigorabowles/Desktop/QA/test_pyramid.jpeg ");
+	// input type = "file" name = "upfile"; you can not automate if you dont have type = "file"
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
